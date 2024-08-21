9158d30d980675d442718ddd71881f1729635873
PedroFilho51 <pedrofilho305@gmail.com>
2024-08-21 16:21:41 -0300

    Demonstração de Tipos primitivos

---
+++ tipos-variaveis/src/ExemplosTiposPrimitivos.java
@@ -0,0 +1,35 @@
+public class ExemplosTiposPrimitivos {
+    public static void main(String[] args) {
+        // Tipo byte
+        byte exemploByte = 100;
+        System.out.println("Valor do byte: " + exemploByte);
+
+        // Tipo short
+        short exemploShort = 10000;
+        System.out.println("Valor do short: " + exemploShort);
+
+        // Tipo int
+        int exemploInt = 100000;
+        System.out.println("Valor do int: " + exemploInt);
+
+        // Tipo long
+        long exemploLong = 100000L;
+        System.out.println("Valor do long: " + exemploLong);
+
+        // Tipo float
+        float exemploFloat = 10.5f;
+        System.out.println("Valor do float: " + exemploFloat);
+
+        // Tipo double
+        double exemploDouble = 10.5;
+        System.out.println("Valor do double: " + exemploDouble);
+
+        // Tipo char
+        char exemploChar = 'A';
+        System.out.println("Valor do char: " + exemploChar);
+
+        // Tipo boolean
+        boolean exemploBoolean = true;
+        System.out.println("Valor do boolean: " + exemploBoolean);
+    }
+}
